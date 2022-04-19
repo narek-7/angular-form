7532ba8f0ea65e05c5b24581b64c31d699838250
Narek <narek1985ch@gmail.com>
2022-04-20 00:52:35 +0200

    the form works

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,32 +8,74 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class AppComponent implements OnInit {
   form!: FormGroup;
-  dismissType: boolean = false;
+
+  closeConsentBar: boolean = false;
+  dismissText: string = '';
+  isDisgusting: boolean = false;
+  dismissType: string = 'cross';
 
   @ViewChild('dismiss', { static: false }) inputRef!: ElementRef;
 
   ngOnInit() {
     this.form = new FormGroup({
       dataLayer: new FormControl(),
-      darkMode: new FormControl(),
+      darkMode: new FormControl(null),
       primaryColor: new FormControl('#146EFF', [Validators.required]),
-      borderRadius: new FormControl(null, [Validators.required]),
-      dismissable: new FormControl(false),
-      dismissType: new FormControl(''),
+      borderRadius: new FormControl('', [Validators.required]),
+      dismissable: new FormControl(true),
+      dismissType: new FormControl('cross'),
       expiration: new FormControl('', [Validators.required]),
       closeType: new FormControl(''),
     });
   }
 
-  hadleDismissType() {
-    this.dismissType = !this.dismissType;
+  hadleClose() {
+    const formData = { ...this.form.value };
+    if (formData.expiration) {
+      this.inputRef.nativeElement.style.display = 'none';
+    }
+  }
+
+  handleDismissType() {
+    if (this.dismissText) {
+      this.dismissType = this.dismissText;
+    } else if (this.isDisgusting) {
+      this.dismissType = 'cross-faint';
+    } else {
+      this.dismissType = 'cross';
+    }
+  }
+
+  hadleDisgusting() {
+    this.isDisgusting = !this.isDisgusting;
+    this.dismissText = '';
+  }
+
+  hadleDismissText(event: any) {
+    this.dismissText = event.target.value;
   }
 
   submit() {
+    const formData = { ...this.form.value };
+
     if (this.form.valid) {
-      console.log('Form: ', this.form);
-      const formData = { ...this.form.value };
-      console.log('Form Data:', formData);
+      this.handleDismissType();
+      if (formData.darkMode === null) {
+        delete formData.darkMode;
+      } else {
+        formData.darkMode = !!formData.darkMode;
+      }
+      if (!formData.closeType) {
+        delete formData.closeType;
+      }
+
+      if (!formData.dataLayer) {
+        delete formData.dataLayer;
+      }
+      formData.dismissable = this.dismissType === 'cross' ? true : false;
+      formData.dismissType = this.dismissType;
+
+      console.log(formData);
     }
   }
 }
